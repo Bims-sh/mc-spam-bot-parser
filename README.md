156d1a2e1db268c49e56d6cb3c331822f7737290
DasIschBims <46683337+DasIschBims@users.noreply.github.com>
2023-06-28 17:55:52 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# mc-spam-bots
-This repository has a script to extract the spammer bots that constantly plague the logs of many minecraft servers
+# Spam Bot Log Parser
+This repository contains a script that is designed to extract and identify spammer bots that frequently appear in the logs of Minecraft servers. The script parses the log file, filters out the spam bot entries, and provides information about the unique IP addresses associated with the spammer bots along with their corresponding names.
+
+## How to
+Usage:
+
+```bash
+./spam-bot-parser.sh <log_file> [options]
+```
+
+Options:
+
+- `-u`, `--unique`: Display only the unique IP addresses and their corresponding names.
+- `-p`, `--ports`: Include the port numbers in the IP addresses.
+
+Example usage:
+
+```bash
+./spam_bot_parser.sh ./logs/latest.log -u
+```
+
+Example output:
+```csv
+Bunger;3.xx.xx.xxx
+Bunger;52.xx.xx.xxx
+cuute;162.xx.xxx.xxx
+cuute;45.xxx.xxx.xxx
+```
+
+Note: If you want to save the output, it can be easily done by using the `>` operator as shown below.
+
+```bash
+./log_parser.sh ./logs/latest.log -u > output.csv
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ spam-bot-parser.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Please provide the path to the log file."
+    exit 1
+fi
+
+log_file_path=""
+unique_flag=false
+ports_flag=false
+
+# Process command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -u|--unique)
+        unique_flag=true
+        shift
+        ;;
+        -p|--ports)
+        ports_flag=true
+        shift
+        ;;
+        *)
+        log_file_path=$1
+        shift
+        ;;
+    esac
+done
+
+# i love regex!
+filter_pattern=".*id=<null>.*"
+name_pattern="name=([[:alnum:]]+)"
+ip_pattern="/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+)"
+# actually, i hate it with every fiber of my being...
+
+if [ -z "$log_file_path" ]; then
+    echo "Please provide the path to the log file."
+    exit 1
+fi
+
+if [ ! -f "$log_file_path" ]; then
+    echo "Log file not found: $log_file_path"
+    exit 1
+fi
+
+if [[ $log_file_path == *.gz ]]; then
+    log_content=$(gunzip -c "$log_file_path")
+else
+    log_content=$(cat "$log_file_path")
+fi
+
+# regex magic
+filtered=$(echo "$log_content" | grep -Po "$filter_pattern")
+names=$(echo "$filtered" | grep -Po "$name_pattern" | awk -F= '{print $2}')
+ips=$(echo "$filtered" | grep -Po "$ip_pattern")
+
+IFS=$'\n' read -rd '' -a name_array <<< "$names"
+IFS=$'\n' read -rd '' -a ip_array <<< "$ips"
+
+output=""
+
+# arrang the output as "name;ip:port" for later use in csv files
+for index in "${!name_array[@]}"; do
+    name=${name_array[index]}
+    ip=${ip_array[index]}
+    
+    if [ "$ports_flag" = true ]; then
+        output+="$name;$ip"$'\n'
+    else
+        ip_without_port=${ip%:*}
+        output+="$name;$ip_without_port"$'\n'
+    fi
+done
+
+if [ "$unique_flag" = true ]; then
+    unique_output=$(echo "$output" | sort -u)
+    trimmed_output=$(echo "$unique_output" | sed '/^$/d')
+    echo "$trimmed_output"
+else
+    echo "$output"
+fi
